f1cd65abbca4fcb9460c6bb039cb19ca7e319bbe
Tanish2122 <tanish2122@github.com>
2026-04-10 20:15:39 +0530

    feat: add doctor profile data and interface definition

--- balaji-hospital-nextjs/src/data/doctors.ts
+++ balaji-hospital-nextjs/src/data/doctors.ts
@@ -21,7 +21,7 @@ export const doctors: Doctor[] = [
     name: "Dr. Ramesh Agarwal",
     phone: "918949518353@c.us",
     specialty: "Chairman & Senior Orthopaedic Surgeon",
-    image: "/images/doctors/Gemini_Generated_Image_8hekj68hekj68hek.png",
+    image: "/images/doctors/ramesh-agarwal.png",
     experience: "30+ Years",
     department: "Orthopedic",
     description:
